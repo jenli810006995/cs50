20d8964d01d2b238ce4dce5614d43e2105bdc6b2
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-06-06 13:24:05 -0400

    Update example.c

--- macro/Parameterized Macros/example.c
+++ macro/Parameterized Macros/example.c
@@ -1,8 +1,21 @@
+#include <stdio.h>
+
 int square(int x) {
    return x * x;
 }
 
+
 #define square(x) ((x) * (x))
+
+
+int main(void)
+{
+    int x = 9;
+    printf("%d", square(x));
+    return 0;
+}
+
+
 
 
 
