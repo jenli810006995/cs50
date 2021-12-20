7f5bc937da1640a626b48ca43fa10289333f2ec9
Joanne <38231830+jenli810006995@users.noreply.github.com>
2021-12-20 11:57:54 -0600

    Create caesar_note.c

---
+++ pset2/caesar_note.c
@@ -0,0 +1,12 @@
+// argc - argument count, aka # of arguments passed to the program
+// argv = argument vector, aka 1 dimensional array of strings
+
+$ ./caesar 1 x z
+argc = 4
+argv = "./caesar", "1", "x", "z"
+argv[0] = ./caesar
+argv[1] = 1
+argv[2] = x
+argv[3] = z
+
+
